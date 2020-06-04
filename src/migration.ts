fc0af9fc337c1a26ad10879bfa577eb78ee8b7f7
Zaenul Hilmi <zaenulhilmi@gmail.com>
2020-06-05 06:40:13 +0700

    repair migration for postgres

--- src/migration.ts
+++ src/migration.ts
@@ -1,11 +1,10 @@
 import MigrationInterface from "./../interfaces/migration_interface.ts";
-import mysql from "./../driver/mysql.ts";
 import MySqlSchema from "./mysql_schema.ts";
 import SchemaInterface from "./../interfaces/schema_interface.ts";
 import Configuration from "./../configuration.ts";
 import MysqlSchemaRepository from "./mysql_schema_repository.ts";
 import MigrationRepositoryInterface from "../interfaces/migration_repository_interface.ts";
-import MysqlMigrationRepository from "./mysql_migration_repository.ts";
+import SchemaFactory from "../schema_factory.ts";
 
 interface MigrationData {
   id?: number;
@@ -18,9 +17,19 @@ interface MigrationData {
 class MysqlMigration implements MigrationInterface {
   data: Array<MigrationData> = [];
   migrationRepo: MigrationRepositoryInterface;
+  dialect: string;
 
   constructor(migrationRepo: MigrationRepositoryInterface) {
     this.migrationRepo = migrationRepo;
+    if (this.migrationRepo.constructor.name == "MysqlMigrationRepository") {
+      this.dialect = "mysql";
+    } else if (
+      this.migrationRepo.constuctor.name == "PostgresMigrationRepository"
+    ) {
+      this.dialect = "postgres";
+    } else {
+      throw new Error(`migration repository doesn't exist`);
+    }
   }
 
   async migrate(): Promise<void> {
@@ -99,10 +108,10 @@ class MysqlMigration implements MigrationInterface {
     for await (let x of this.data) {
       let Class =
         (await import(`${projectDir}/${migrationDir}/${x.file_name}`)).default;
-      let mysqlSchemaRepository = new MySqlSchema(new MysqlSchemaRepository());
+      let schema: SchemaInterface = new SchemaFactory(this.dialect).get()
       let object = new Class();
       await this.migrationRepo.insert(x.file_name, x.step).execute();
-      await object.up(mysqlSchemaRepository);
+      await object.up(schema);
     }
   }
 
@@ -129,9 +138,9 @@ class MysqlMigration implements MigrationInterface {
     for await (let x of lastStepMigrations) {
       let Class =
         (await import(`${projectDir}/${migrationDir}/${x.file_name}`)).default;
-      let mysqlSchemaRepository = new MySqlSchema(new MysqlSchemaRepository());
+      let schema: SchemaInterface = new SchemaFactory(this.dialect).get
       let object = new Class();
-      await object.down(mysqlSchemaRepository);
+      await object.down(schema);
     }
     await this.migrationRepo.removeAllLastStep().execute();
   }
